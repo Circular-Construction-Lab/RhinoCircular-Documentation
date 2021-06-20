76ad035a60a79194a9def931eccbd3a5d8d65d70
Hongjun Wu <hongjunwu@hotmail.com>
2021-06-20 01:41:23 -0700

    Initial site generation

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://pmarsceill.github.io/rc-documentation/assets/js/search-data.json', true);
+  request.open('GET', 'https://circular-construction-lab.github.io/RhinoCircular-Documentation//assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://pmarsceill.github.io/rc-documentation/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://circular-construction-lab.github.io/RhinoCircular-Documentation/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
